fecaf19805233f0b9d16314a8dcdc0085e0ed97d
Kyriakos Paschalidis <kyriakos.paschalidis@gmail.com>
2018-10-25 13:28:16 +0300

    Make vue component form reactive

--- resources/js/components/CreatePost.vue
+++ resources/js/components/CreatePost.vue
@@ -1,27 +1,32 @@
 <template>
     <form class="mt-20" action="" method="POST">
-        
+
         <label class="block font-bold mb-2">Title</label>
-        <input type="text" name="title" value="" class="border-2 border-grey block w-1/2 mb-10 px-3 py-2 rounded">
+        <input v-model="title" type="text" name="title" class="border-2 border-grey block w-1/2 mb-10 px-3 py-2 rounded">
 
         <label class="block font-bold mb-2">Author</label>
-        <input type="text" name="author" value="" class="border-2 border-grey block w-1/2 mb-10 px-3 py-2 rounded">
+        <input v-model="author" type="text" name="author" class="border-2 border-grey block w-1/2 mb-10 px-3 py-2 rounded">
 
         <label class="block font-bold mb-2">Date</label>
-        <input type="text" name="date" value="" class="border-2 border-grey block w-1/2 mb-10 px-3 py-2 rounded">
+        <input v-model="date" type="text" name="date" class="border-2 border-grey block w-1/2 mb-10 px-3 py-2 rounded">
 
         <label class="block font-bold mb-2">Body</label>
-        <textarea name="body" rows="10" class="border-2 border-grey block w-1/2 mb-10 px-3 py-2 rounded"></textarea>
+        <textarea v-model="body" name="body" rows="10" class="border-2 border-grey block w-1/2 mb-10 px-3 py-2 rounded"></textarea>
         
-        <button type="submit" class="rounded px-4 py-3 bg-green text-white no-underline" href="">Create Post</button>
+        <button class="rounded px-4 py-3 bg-green text-white no-underline">Create Post</button>
         <a class="rounded px-4 py-3 bg-grey-dark text-white no-underline" href="">Back to All Posts</a>
     </form> 
 </template>
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+        data() {
+            return {
+                title: null,
+                author: null,
+                date: null,
+                body: null,
+            }
+        },
     }
 </script>
