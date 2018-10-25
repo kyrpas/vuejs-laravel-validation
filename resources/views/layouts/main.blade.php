a919e7d91146df14a7a608d0c552b51632e373c3
Kyriakos Paschalidis <kyriakos.paschalidis@gmail.com>
2018-10-25 11:54:10 +0300

    Extract basic blade layout and apply it to welcome view

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+        <title>Posts</title>
+    </head>
+    <body class="pt-24">
+        <div class="container mx-auto">
+            @yield('content')
+        </div>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,5 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
-        <title>Posts</title>
-    </head>
-    <body class="pt-24">
-        <div class="container mx-auto">
-            <h1 class="border-b">Posts</h1>
-        </div>
-    </body>
-</html>
+@extends('layouts.main')
+
+@section('content')
+    <h1 class="border-b">Posts</h1>
+@stop
