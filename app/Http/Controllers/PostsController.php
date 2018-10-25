f7b13d14ae154460d8c331bb9c3cf660ed86fab7
Kyriakos Paschalidis <kyriakos.paschalidis@gmail.com>
2018-10-25 12:39:04 +0300

    Update index page to show all stored posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,7 +9,8 @@ class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
     }
 
     public function create()
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,4 +2,11 @@
 
 @section('content')
     <h1 class="border-b">Posts</h1>
+
+    @foreach($posts as $post)
+        <div class="mt-10">
+            <h3 class="mb-4"><a class="text-green-dark no-underline" href="#">{{ $post->title }}</a></h4>
+            <p class="leading-normal">{{ str_limit($post->body, 300) }}</p>
+        </div>
+    @endforeach
 @stop
